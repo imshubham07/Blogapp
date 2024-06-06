6afe18dc9f16ef514755d9c79aaf9d2100ee9b41
imshubham07 <mrshubhamdubey20@gmail.com>
2024-06-06 17:14:01 +0530

    Blog app Complete

--- src/components/post-form/PostForm.jsx
+++ src/components/post-form/PostForm.jsx
@@ -17,7 +17,6 @@ export default function PostForm({ post }) {
 
     const navigate = useNavigate();
     const userData = useSelector((state) => state.auth.userData);
-    // console.log(userData);
 
     const submit = async (data) => {
         if (post) {
@@ -40,9 +39,8 @@ export default function PostForm({ post }) {
 
             if (file) {
                 const fileId = file.$id;
-                // console.log(userData.$id);
                 data.featuredImage = fileId;
-                const dbPost = await appwriteService.createPost({ ...data, userId:  userData.$id});
+                const dbPost = await appwriteService.createPost({ ...data, userId: userData.$id });
 
                 if (dbPost) {
                     navigate(`/post/${dbPost.$id}`);
